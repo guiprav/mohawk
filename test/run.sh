660c5b8aac2ac89bde268af4d7b464966e336898
Guilherme Vieira <super.driver.512@gmail.com>
2015-11-19 00:50:28 -0200

    test/run.sh: Pass file and pipe mode consolidated test outputs to check.js.

--- test/run.sh
+++ test/run.sh
@@ -3,9 +3,9 @@ set -e
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-echo File mode:
-(cd ../ && mohawk test/index.hbs | test/check.js)
-
-echo
-echo Pipe mode:
-mohawk <index.hbs | ./check.js
+cat \
+    <(echo File mode:) \
+    <(cd ../ && mohawk test/index.hbs) \
+    <(echo; echo Pipe mode:) \
+    <(mohawk <index.hbs) \
+    | ./check.js
